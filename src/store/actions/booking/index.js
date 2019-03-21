a54e1d5fb869d172b70b59ac28d1f139e7da1e3d
danijel20171111 <danijel20171111@gmail.com>
2019-03-22 03:43:18 +0800

    booking action clean

--- src/store/actions/booking/index.js
+++ src/store/actions/booking/index.js
@@ -2,11 +2,12 @@
  * Description: Booking Layout Actions
  * Date: 3/20/2019
  */
+
 import * as Utils from 'utils';
 
-export const SERVICES_REQUEST = '[BOOKING] SERVICES REQUEST';
-export const SERVICES_SUCCESS = '[BOOKING] SERVICES SUCCESS';
-export const SERVICES_FAILED = '[BOOKING] SERVICES FAILED';
+export const SERVICES_REQUEST   = '[BOOKING] SERVICES REQUEST';
+export const SERVICES_SUCCESS   = '[BOOKING] SERVICES SUCCESS';
+export const SERVICES_FAILED    = '[BOOKING] SERVICES FAILED';
 
 export function getBookingServices(data) {
     const request = Utils.xapi().post('booking/services', data);
@@ -17,21 +18,21 @@ export function getBookingServices(data) {
         })
         request.then((response) =>
             dispatch({
-                type   : SERVICES_SUCCESS,
-                payload: response.data.services
+                type    : SERVICES_SUCCESS,
+                payload : response.data.services
             })
         ).catch((error) => {
             dispatch({
-                type   : SERVICES_FAILED,
-                payload: error
+                type    : SERVICES_FAILED,
+                payload : error
             })
         });
     }        
 }
 
-export const EMPLOYEES_REQUEST = '[BOOKING] EMPLOYEES REQUEST';
-export const EMPLOYEES_SUCCESS = '[BOOKING] EMPLOYEES SUCCESS';
-export const EMPLOYEES_FAILED = '[BOOKING] EMPLOYEES FAILED';
+export const EMPLOYEES_REQUEST  = '[BOOKING] EMPLOYEES REQUEST';
+export const EMPLOYEES_SUCCESS  = '[BOOKING] EMPLOYEES SUCCESS';
+export const EMPLOYEES_FAILED   = '[BOOKING] EMPLOYEES FAILED';
 
 export function getBookingEmployees(data) {
     const request = Utils.xapi().post('booking/hairdressers', data);
@@ -42,21 +43,21 @@ export function getBookingEmployees(data) {
         })
         request.then((response) =>
             dispatch({
-                type   : EMPLOYEES_SUCCESS,
-                payload: response.data.Employees
+                type    : EMPLOYEES_SUCCESS,
+                payload : response.data.Employees
             })
         ).catch((error) => {
             dispatch({
-                type   : EMPLOYEES_FAILED,
-                payload: error
+                type    : EMPLOYEES_FAILED,
+                payload : error
             })
         });
     }
 }
 
-export const DAYSOFF_REQUEST = '[BOOKING] DAYSOFF REQUEST';
-export const DAYSOFF_SUCCESS = '[BOOKING] DAYSOFF SUCCESS';
-export const DAYSOFF_FAILED = '[BOOKING] DAYSOFF FAILED';
+export const DAYSOFF_REQUEST    = '[BOOKING] DAYSOFF REQUEST';
+export const DAYSOFF_SUCCESS    = '[BOOKING] DAYSOFF SUCCESS';
+export const DAYSOFF_FAILED     = '[BOOKING] DAYSOFF FAILED';
 
 export function getBookingDaysOff(data) {
     const request = Utils.xapi().post('booking/daysoff', data);
@@ -67,21 +68,21 @@ export function getBookingDaysOff(data) {
         })
         request.then((response) =>
             dispatch({
-                type   : DAYSOFF_SUCCESS,
-                payload: response.data
+                type    : DAYSOFF_SUCCESS,
+                payload : response.data
             })
         ).catch((error) => {
             dispatch({
-                type   : DAYSOFF_FAILED,
-                payload: error
+                type    : DAYSOFF_FAILED,
+                payload : error
             })
         });
     }
 }
 
-export const TIMESLOTS_REQUEST = '[BOOKING] TIMESLOTS REQUEST';
-export const TIMESLOTS_SUCCESS = '[BOOKING] TIMESLOTS SUCCESS';
-export const TIMESLOTS_FAILED = '[BOOKING] TIMESLOTS FAILED';
+export const TIMESLOTS_REQUEST  = '[BOOKING] TIMESLOTS REQUEST';
+export const TIMESLOTS_SUCCESS  = '[BOOKING] TIMESLOTS SUCCESS';
+export const TIMESLOTS_FAILED   = '[BOOKING] TIMESLOTS FAILED';
 
 export function getBookingTimeslots(data) {
     const request = Utils.xapi().post('booking/timeslots', data);
@@ -92,13 +93,13 @@ export function getBookingTimeslots(data) {
         })
         request.then((response) =>
             dispatch({
-                type   : TIMESLOTS_SUCCESS,
-                payload: response.data
+                type    : TIMESLOTS_SUCCESS,
+                payload : response.data
             })
         ).catch((error) => {
             dispatch({
-                type   : TIMESLOTS_FAILED,
-                payload: error
+                type    : TIMESLOTS_FAILED,
+                payload : error
             })
         });
     }
--- src/store/reducers/booking/index.js
+++ src/store/reducers/booking/index.js
@@ -6,96 +6,95 @@
 import * as Actions from './../../actions';
 
 const initialState = {
-    loading: false,
-    error: '',
-    services: [],
-    employees: [],
-    daysOff: null,
-    timeSlots: [],
+    loading     : false,
+    error       : '',
+    services    : [],
+    employees   : [],
+    daysOff     : null,
+    timeSlots   : []
 };
 
 const booking = function (state = initialState, action) {
-    switch ( action.type )
-    {
+    switch ( action.type ) {
         case Actions.SERVICES_REQUEST:
             return {
                 ...state,
-                loading: true,
-                error: '',
-                services: []
+                loading     : true,
+                error       : '',
+                services    : []
             };
         case Actions.SERVICES_SUCCESS:
             return {
                 ...state,
-                loading: false,
-                services: action.payload
+                loading     : false,
+                services    : action.payload
             };
         case Actions.SERVICES_FAILED:
             return {
                 ...state,
-                loading: false,
-                error: action.payload
+                loading     : false,
+                error       : action.payload
             };
 
         case Actions.EMPLOYEES_REQUEST:
             return {
                 ...state,
-                loading: true,
-                error: '',
-                employees: []
+                loading     : true,
+                error       : '',
+                employees   : []
             };
         case Actions.EMPLOYEES_SUCCESS:
             return {
                 ...state,
-                loading: false,
-                employees: action.payload
+                loading     : false,
+                employees   : action.payload
             };
         case Actions.EMPLOYEES_FAILED:
             return {
                 ...state,
-                loading: false,
-                error: action.payload
+                loading     : false,
+                error       : action.payload
             };
 
         case Actions.DAYSOFF_REQUEST:
             return {
                 ...state,
-                loading: true,
-                error: '',
-                daysOff: null,
-                timeSlots: []
+                loading     : true,
+                error       : '',
+                daysOff     : null,
+                timeSlots   : []
             };
         case Actions.DAYSOFF_SUCCESS:
             return {
                 ...state,
-                loading: false,
-                daysOff: action.payload
+                loading     : false,
+                daysOff     : action.payload
             };
         case Actions.DAYSOFF_FAILED:
             return {
                 ...state,
-                loading: false,
-                error: action.payload
+                loading     : false,
+                error       : action.payload
             };
 
         case Actions.TIMESLOTS_REQUEST:
             return {
                 ...state,
-                loading: false,
-                error: '',
-                timeSlots: []
+                loading     : false,
+                error       : '',
+                timeSlots   : []
             };
         case Actions.TIMESLOTS_SUCCESS:
             return {
                 ...state,
-                loading: false,
-                timeSlots: action.payload
+                loading     : false,
+                timeSlots   : action.payload
             };
         case Actions.TIMESLOTS_FAILED:
             return {
                 ...state,
-                loading: false,
-                error: action.payload
+                loading     : false,
+                error       : action.payload
             };
         default:
             return state;
