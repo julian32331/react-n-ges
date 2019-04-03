1ef643c4f401bf48d2a56554451dd297a71e6bd8
danijel20171111 <danijel20171111@gmail.com>
2019-04-03 17:20:09 +0800

    add some categories

--- src/views/B2BShop/Products/Category.jsx
+++ src/views/B2BShop/Products/Category.jsx
@@ -29,7 +29,54 @@ const categories = [
                         deep: 2
                     }
                 ]
-            }
+            },
+            {
+                collapse: true,
+                name: "Keratin",
+                deep: 1,
+                children: [
+                    {
+                        name: "Äkta hår Rakt",
+                        deep: 2
+                    },
+                    {
+                        name: "Äkta Hår Lockigt",
+                        deep: 2
+                    },
+                    {
+                        name: "Syntethår",
+                        deep: 2
+                    },
+                    {
+                        name: "Fiberhår",
+                        deep: 2
+                    },
+                    {
+                        name: "Dreads",
+                        deep: 2
+                    },
+                    {
+                        name: "Swarovski",
+                        deep: 2
+                    },
+                    {
+                        name: "Tillbehör",
+                        deep: 2
+                    }
+                ]
+            },
+            {
+                name: "Tränsar",
+                deep: 1
+            },
+            {
+                name: "Cold System",
+                deep: 1
+            },
+            {
+                name: "Tillbehör",
+                deep: 1
+            },
         ]
     },
     {
@@ -59,7 +106,117 @@ const categories = [
                         deep: 2
                     },
                 ]
-            }
+            },
+            {
+                collapse: true,
+                name: "Styling",
+                deep: 1,
+                children: [
+                    {
+                        name: "Spray",
+                        deep: 2
+                    },
+                    {
+                        name: "Stylingspray",
+                        deep: 2
+                    },
+                    {
+                        name: "Volymspray",
+                        deep: 2
+                    },
+                    {
+                        name: "Finishingspray",
+                        deep: 2
+                    },
+                    {
+                        name: "Glansspray",
+                        deep: 2
+                    },
+                    {
+                        name: "Hårvax",
+                        deep: 2
+                    },
+                    {
+                        name: "Vax",
+                        deep: 2
+                    },
+                    {
+                        name: "Paste",
+                        deep: 2
+                    },
+                    {
+                        name: "Clay",
+                        deep: 2
+                    },
+                    {
+                        name: "Fibervax",
+                        deep: 2
+                    },
+                    {
+                        name: "Gel",
+                        deep: 2
+                    },
+                    {
+                        name: "Torrschampo",
+                        deep: 2
+                    },
+                    {
+                        name: "Värmeskydd",
+                        deep: 2
+                    },
+                    {
+                        name: "Saltvattenspray",
+                        deep: 2
+                    },
+                    {
+                        name: "Olja/Serum",
+                        deep: 2
+                    },
+                    {
+                        name: "Creme",
+                        deep: 2
+                    },
+                    {
+                        name: "Volym",
+                        deep: 2
+                    },
+                    {
+                        name: "Mousse",
+                        deep: 2
+                    },
+                ]
+            },
+            {
+                collapse: true,
+                name: "Hårfärg",
+                deep: 1,
+                children: [
+                    {
+                        name: "Temporära Färger",
+                        deep: 2
+                    },
+                    {
+                        name: "Sprayfärg",
+                        deep: 2
+                    },
+                    {
+                        name: "Cover Grey",
+                        deep: 2
+                    },
+                    {
+                        name: "Hair Shadow",
+                        deep: 2
+                    },
+                    {
+                        name: "Färginpackningar",
+                        deep: 2
+                    },
+                ]
+            },
+            {
+                name: "Skäggvård",
+                deep: 1
+            },
         ]
     }
 ];
