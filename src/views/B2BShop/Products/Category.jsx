c116398c8bc1354530a4b83790eba5803a913d20
danijel20171111 <danijel20171111@gmail.com>
2019-04-04 01:32:25 +0800

    category completed

--- src/views/B2BShop/Products/Category.jsx
+++ src/views/B2BShop/Products/Category.jsx
@@ -318,6 +318,515 @@ const categories = [
                 deep: 1
             },
         ]
+    },
+    {
+        collapse: true,
+        name: "Tillbehör/Förbrukning",
+        deep: 0,
+        children: [
+            {
+                collapse: true,
+                name: "Färg- och permanenttillbehör",
+                deep: 1,
+                children: [
+                    {
+                        name: "Folie",
+                        deep: 2
+                    },
+                    {
+                        name: "Färgskålar",
+                        deep: 2
+                    },
+                    {
+                        name: "Penslar",
+                        deep: 2
+                    },
+                    {
+                        name: "Handskar",
+                        deep: 2
+                    },
+                    {
+                        name: "Hättor",
+                        deep: 2
+                    },
+                    {
+                        name: "Permanentspolar",
+                        deep: 2
+                    },
+                    {
+                        name: "Övrigt",
+                        deep: 2
+                    },
+                ]
+            },
+            {
+                name: "Klippkappor",
+                deep: 1
+            },
+            {
+                name: "Förkläden",
+                deep: 1
+            },
+            {
+                name: "Handdukar",
+                deep: 1
+            },
+            {
+                collapse: true,
+                name: "Accessoarer",
+                deep: 1,
+                children: [
+                    {
+                        name: "Klämmor",
+                        deep: 2
+                    },
+                    {
+                        name: "Nålar",
+                        deep: 2
+                    },
+                    {
+                        name: "Snoddar",
+                        deep: 2
+                    },
+                    {
+                        name: "Hårsmycken",
+                        deep: 2
+                    }
+                ]
+            },
+            {
+                name: "Vattenflaskor",
+                deep: 1
+            },
+            {
+                name: "Speglar",
+                deep: 1
+            },
+            {
+                name: "Övningshuvud",
+                deep: 1
+            },
+            {
+                name: "Arbetskläder",
+                deep: 1
+            },
+            {
+                name: "Stödstrumpor",
+                deep: 1
+            },
+            {
+                name: "Städartiklar",
+                deep: 1
+            },
+            {
+                name: "Böcker, Posters, Reklam",
+                deep: 1
+            },
+            {
+                name: "Övrigt",
+                deep: 1
+            },
+        ]
+    },
+    {
+        collapse: true,
+        name: "Inredning",
+        deep: 0,
+        children: [
+            {
+                name: "Arbetsbord",
+                deep: 1
+            },
+            {
+                name: "Arbetsstolar",
+                deep: 1
+            },
+            {
+                name: "Barber Chairs",
+                deep: 1
+            },
+            {
+                name: "Belysning",
+                deep: 1
+            },
+            {
+                name: "Kundplatser",
+                deep: 1
+            },
+            {
+                name: "Kundstolar",
+                deep: 1
+            },
+            {
+                name: "Massageschamponeringar",
+                deep: 1
+            },
+            {
+                name: "Massage & Behandlingsbänkar",
+                deep: 1
+            },
+            {
+                name: "Nagelbord, Beautyutrustning",
+                deep: 1
+            },
+            {
+                name: "Receptionsdiskar",
+                deep: 1
+            },
+            {
+                name: "Schamponeringar",
+                deep: 1
+            },
+            {
+                name: "Tatueringsinredning",
+                deep: 1
+            },
+            {
+                name: "Väntplatser",
+                deep: 1
+            },
+            {
+                name: "Reservdelar",
+                deep: 1
+            },
+            {
+                name: "Övrigt",
+                deep: 1
+            },
+        ]
+    },
+    {
+        collapse: true,
+        name: "Verktyg",
+        deep: 0,
+        children: [
+            {
+                name: "Fönar",
+                deep: 1
+            },
+            {
+                name: "Plattänger",
+                deep: 1
+            },
+            {
+                name: "Locktänger & Varmluftsborstar",
+                deep: 1
+            },
+            {
+                name: "Klippmaskiner & Trimmer",
+                deep: 1
+            },
+            {
+                name: "Torkhuvar & Klimazoner",
+                deep: 1
+            },
+            {
+                name: "Borstar",
+                deep: 1
+            },
+            {
+                name: "Kammar",
+                deep: 1
+            },
+            {
+                collapse: true,
+                name: "Saxar & Knivar",
+                deep: 1,
+                children: [
+                    {
+                        name: "Klippsaxar",
+                        deep: 2
+                    },
+                    {
+                        name: "Effilersaxar",
+                        deep: 2
+                    },
+                    {
+                        name: "Knivar",
+                        deep: 2
+                    },
+                    {
+                        name: "Skolset / Paketerbjudanden",
+                        deep: 2
+                    },
+                    {
+                        name: "Tillbehör",
+                        deep: 2
+                    }
+                ]
+            },
+            {
+                name: "Grooming, rakningstillbehör",
+                deep: 1
+            },
+            {
+                name: "Hölster & Väskor",
+                deep: 1
+            },
+            {
+                name: "Tillbehör",
+                deep: 1
+            }
+        ]
+    },
+    {
+        collapse: true,
+        name: "Beauty",
+        deep: 0,
+        children: [
+            {
+                collapse: true,
+                name: "Ögonbryn & Fransar",
+                deep: 1,
+                children: [
+                    {
+                        name: "Färg",
+                        deep: 2
+                    },
+                    {
+                        name: "Permanent",
+                        deep: 2
+                    },
+                    {
+                        name: "Ögonfransnäring",
+                        deep: 2
+                    }
+                ]
+            },
+            {
+                collapse: true,
+                name: "Hudvård",
+                deep: 1,
+                children: [
+                    {
+                        name: "Ansiktsvård",
+                        deep: 2
+                    },
+                    {
+                        name: "Kroppsvård",
+                        deep: 2
+                    },
+                    {
+                        name: "Herrprodukter",
+                        deep: 2
+                    },
+                    {
+                        name: "Specialprodukter",
+                        deep: 2
+                    },
+                    {
+                        name: "Parfym",
+                        deep: 2
+                    }
+                ]
+            },
+            {
+                collapse: true,
+                name: "Naglar",
+                deep: 1,
+                children: [
+                    {
+                        name: "Tippar",
+                        deep: 2
+                    },
+                    {
+                        name: "Penslar",
+                        deep: 2
+                    },
+                    {
+                        name: "Filar",
+                        deep: 2
+                    },
+                    {
+                        name: "UV-Lampor",
+                        deep: 2
+                    },
+                    {
+                        name: "Nagellack",
+                        deep: 2
+                    },
+                    {
+                        name: "Nail-art",
+                        deep: 2
+                    },
+                    {
+                        name: "Stenar & Kristaller",
+                        deep: 2
+                    },
+                    {
+                        name: "3D Nail Stickers",
+                        deep: 2
+                    },
+                    {
+                        name: "Filtersystem",
+                        deep: 2
+                    },
+                    {
+                        name: "Nageltillbehör",
+                        deep: 2
+                    }
+                ]
+            },
+            {
+                collapse: true,
+                name: "Make up",
+                deep: 1,
+                children: [
+                    {
+                        name: "Mascara",
+                        deep: 2
+                    },
+                    {
+                        name: "Ögonskugga",
+                        deep: 2
+                    },
+                    {
+                        name: "Puder & Foundation",
+                        deep: 2
+                    },
+                    {
+                        name: "Rouge",
+                        deep: 2
+                    },
+                    {
+                        name: "Eyeliner & Kajal",
+                        deep: 2
+                    },
+                    {
+                        name: "Läppstift",
+                        deep: 2
+                    },
+                    {
+                        name: "Lipliner",
+                        deep: 2
+                    },
+                    {
+                        name: "Tillbehör",
+                        deep: 2
+                    }
+                ]
+            },
+            {
+                name: "Massage",
+                deep: 1
+            },
+            {
+                name: "Pedikyr",
+                deep: 1
+            },
+            {
+                collapse: true,
+                name: "Öronhåltagning",
+                deep: 1,
+                children: [
+                    {
+                        name: "Öronhåltagning",
+                        deep: 2
+                    },
+                    {
+                        name: "Hudvänliga smycken",
+                        deep: 2
+                    },
+                    {
+                        name: "Tillbehör",
+                        deep: 2
+                    }
+                ]
+            },
+            {
+                collapse: true,
+                name: "Näshåltagning",
+                deep: 1,
+                children: [
+                    {
+                        name: "Klassiskt vax",
+                        deep: 2
+                    },
+                    {
+                        name: "Frukt wax",
+                        deep: 2
+                    },
+                    {
+                        name: "Vax för känslig hud",
+                        deep: 2
+                    },
+                    {
+                        name: "Brazillian vax",
+                        deep: 2
+                    },
+                    {
+                        name: "Creme & Stripes",
+                        deep: 2
+                    },
+                    {
+                        name: "Vaxvärmare och Tillbehör",
+                        deep: 2
+                    },
+                    {
+                        name: "Förbrukning",
+                        deep: 2
+                    }
+                ]
+            },
+            {
+                name: "Paraffinbehandling",
+                deep: 1
+            },
+            {
+                name: "Self Tanning",
+                deep: 1
+            },
+            {
+                name: "Frisörsmycken",
+                deep: 1
+            },
+            {
+                name: "Tandsmycken",
+                deep: 1
+            },
+            {
+                name: "Tassel Tatoos",
+                deep: 1
+            },
+            {
+                name: "Tillbehör",
+                deep: 1
+            },
+            {
+                collapse: true,
+                name: "Ögonfransförlängning",
+                deep: 1,
+                children: [
+                    {
+                        name: "Fransar",
+                        deep: 2
+                    },
+                    {
+                        name: "Lim & Remover",
+                        deep: 2
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        collapse: true,
+        name: "Övrigt",
+        deep: 0,
+        children: [
+            {
+                name: "Trycksaker",
+                deep: 1
+            },
+            {
+                name: "Lagerrensning",
+                deep: 1
+            },
+            {
+                name: "Presenter (gratis)",
+                deep: 1
+            },
+        ]
     }
 ];
 
