e98b657ecea79e3be8a9bacf6bb134a5b11768fd
danijel20171111 <danijel20171111@gmail.com>
2019-04-24 13:44:52 +0800

    common style clean

--- src/assets/jss/material-dashboard-pro-react/views/commonStyle.jsx
+++ src/assets/jss/material-dashboard-pro-react/views/commonStyle.jsx
@@ -1,52 +1,50 @@
 /**
  * Description: Common Style
- * Date: 12/21/2018
+ * Date: 4/23/2019
  */
-import {
-  dangerColor
-} from "assets/jss/material-dashboard-pro-react.jsx";
 
 const commonStyle = {
-    card: {
-      minHeight: '65vh',
-      margin: '0',
-    },
-    cardTitle: {
-      marginTop: "0",
-      color: "#434343",
-      fontSize: '24px',
-      fontFamily: 'Source Sans Pro',
-      fontWeight: '700',
-    },
-    cardHeader: {
-      zIndex: "3",
-      marginTop: "15px",
-    },
-    cardContent: {
-      position: "relative",
-    },
+  // loading styles
+  loading_container: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  loading: {
+    color: '#7da8ae'
+  },
 
-    // loading styles
-    loading_container: {
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-    },
-    loading: {
-      color: '#7da8ae'
-    },
+  // text align
+  left: {
+    textAlign: 'left'
+  },
+  center: {
+    textAlign: 'center'
+  },
+  right: {
+    textAlign: 'right'
+  },
 
-    danger: {
-      color: dangerColor + "!important"
-    },
-    center: {
-      textAlign: 'center'
-    },
-    right: {
-      textAlign: 'right'
-    },
-  };
+  card: {
+    minHeight: '65vh',
+    margin: '0',
+  },
+  cardTitle: {
+    marginTop: "0",
+    color: "#434343",
+    fontSize: '24px',
+    fontFamily: 'Source Sans Pro',
+    fontWeight: '700',
+  },
+  cardHeader: {
+    zIndex: "3",
+    marginTop: "15px",
+  },
+  cardContent: {
+    position: "relative",
+  },
+};
   
 export default commonStyle;
   
