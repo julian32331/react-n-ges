b58b48bab4bf06f0448375f0849450c9b3c3939e
danijel20171111 <danijel20171111@gmail.com>
2019-07-05 17:15:36 +0800

    comment calendar header change

--- src/views/booking_appointment/CutomToolbar.jsx
+++ src/views/booking_appointment/CutomToolbar.jsx
@@ -57,144 +57,197 @@ class CustomToolbar extends React.PureComponent {
         const { onNavigate, onView, classes } = this.props;
         
         return (
-            <GridContainer alignItems="center" className={classes.pb_15}>
-                <GridItem xs={12} sm={4}>
-                    <GridContainer>
-                        <GridItem xs={12} sm={6}>
-                            <FormControl
-                                fullWidth
-                                className={classes.selectFormControl}
-                            >
-                                <Select
-                                    MenuProps={{
-                                        className: classes.selectMenu
-                                    }}
-                                    classes={{
-                                        select: classes.select + " " + classes.text_left
-                                    }}
-                                    value={this.state.employee}
-                                    onChange={this.handleEmployee}
-                                    inputProps={{
-                                        name: "employee",
-                                        id: "employee"
-                                    }}
-                                >
-                                    <MenuItem
-                                        disabled
-                                        classes={{
-                                        root: classes.selectMenuItem
-                                        }}
-                                    >
-                                        Select Hairdresser
-                                    </MenuItem>
-                                    <MenuItem
-                                        classes={{
-                                            root: classes.selectMenuItem,
-                                            selected: classes.selectMenuItemSelected
-                                        }}
-                                        value={0}
-                                    >
-                                        All
-                                    </MenuItem>
-                                    {
-                                        this.props.employees.map((employee, index) => {
-                                            return (
-                                                <MenuItem
-                                                    classes={{
-                                                        root: classes.selectMenuItem,
-                                                        selected: classes.selectMenuItemSelected
-                                                    }}
-                                                    value={employee.hairdresser_id}
-                                                    key={index}
-                                                >
-                                                    {employee.name}
-                                                </MenuItem>
-                                            )
-                                        })
-                                    }
-                                </Select>
-                            </FormControl>
-                        </GridItem>
-                    </GridContainer>
-                </GridItem>
-                <GridItem xs={12} sm={4}>
-                    <GridContainer justify="center" alignItems="center">
-                        <GridItem>
-                            <Button
-                                simple
-                                color="info"
-                                className={classes.actionButton + " " + classes.actionButtonRound}
-                                onClick={() => onNavigate('PREV')}
-                            >
-                                <ArrowBackIos />
-                            </Button>
-                        </GridItem>
-                        {
-                            this.props.view === "day"? (
-                                <GridItem className={classes.pr_0}>
-                                    <FormControl>
-                                        <Datetime
-                                            dateFormat={"YYYY-MM-DD"}
-                                            timeFormat={false}
-                                            value={this.props.label}
-                                            onChange={event => this.handleChange(event)}
-                                            inputProps={{
-                                                style: {
-                                                    fontSize: '18px',
-                                                    backgroundImage: 'none',
-                                                    width: '100px'
-                                                },
-                                                disabled: true
-                                            }}
-                                            open={this.state.showDatePicker}
-                                        />
-                                    </FormControl>
-                                </GridItem>
-                            ) : (
-                                <GridItem>
-                                    <div className={classes.label}>{this.props.label}</div>
-                                </GridItem>
-                            )
-                        }
-                        {
-                            this.props.view === "day" && 
-                                <GridItem className={classes.pl_0}>
-                                    <ClickAwayListener onClickAway={() => this.setState({showDatePicker: false})}>
-                                        <Button
-                                            simple
-                                            color="info"
-                                            className={classes.actionButton + " " + classes.actionButtonRound}
-                                            onClick={() => this.changeDate()}
-                                        >
-                                            <Today />
-                                        </Button>
-                                    </ClickAwayListener>
-                                </GridItem>
-                        }
-                        <GridItem>
-                            <Button
-                                simple
-                                color="info"
-                                className={classes.actionButton + " " + classes.actionButtonRound}
-                                onClick={() => onNavigate('NEXT')}
-                            >
-                                <ArrowForwardIos />
-                            </Button>
-                        </GridItem>
-                    </GridContainer>
-                </GridItem>
-                <GridItem xs={12} sm={4} className={classes.right}>
-                    <Button color="success" round size="sm" onClick={() => onView('month')} className={classes.marginRight}>
-                        Month
+            // <GridContainer alignItems="center" className={classes.pb_15}>
+            //     <GridItem xs={12} sm={4}>
+            //         <GridContainer>
+            //             <GridItem xs={12} sm={6}>
+            //                 <FormControl
+            //                     fullWidth
+            //                     className={classes.selectFormControl}
+            //                 >
+            //                     <Select
+            //                         MenuProps={{
+            //                             className: classes.selectMenu
+            //                         }}
+            //                         classes={{
+            //                             select: classes.select + " " + classes.text_left
+            //                         }}
+            //                         value={this.state.employee}
+            //                         onChange={this.handleEmployee}
+            //                         inputProps={{
+            //                             name: "employee",
+            //                             id: "employee"
+            //                         }}
+            //                     >
+            //                         <MenuItem
+            //                             disabled
+            //                             classes={{
+            //                             root: classes.selectMenuItem
+            //                             }}
+            //                         >
+            //                             Select Hairdresser
+            //                         </MenuItem>
+            //                         <MenuItem
+            //                             classes={{
+            //                                 root: classes.selectMenuItem,
+            //                                 selected: classes.selectMenuItemSelected
+            //                             }}
+            //                             value={0}
+            //                         >
+            //                             All
+            //                         </MenuItem>
+            //                         {
+            //                             this.props.employees.map((employee, index) => {
+            //                                 return (
+            //                                     <MenuItem
+            //                                         classes={{
+            //                                             root: classes.selectMenuItem,
+            //                                             selected: classes.selectMenuItemSelected
+            //                                         }}
+            //                                         value={employee.hairdresser_id}
+            //                                         key={index}
+            //                                     >
+            //                                         {employee.name}
+            //                                     </MenuItem>
+            //                                 )
+            //                             })
+            //                         }
+            //                     </Select>
+            //                 </FormControl>
+            //             </GridItem>
+            //         </GridContainer>
+            //     </GridItem>
+            //     <GridItem xs={12} sm={4}>
+            //         <GridContainer justify="center" alignItems="center">
+            //             <GridItem>
+            //                 <Button
+            //                     simple
+            //                     color="info"
+            //                     className={classes.actionButton + " " + classes.actionButtonRound}
+            //                     onClick={() => onNavigate('PREV')}
+            //                 >
+            //                     <ArrowBackIos />
+            //                 </Button>
+            //             </GridItem>
+            //             {
+            //                 this.props.view === "day"? (
+            //                     <GridItem className={classes.pr_0}>
+            //                         <FormControl>
+            //                             <Datetime
+            //                                 dateFormat={"YYYY-MM-DD"}
+            //                                 timeFormat={false}
+            //                                 value={this.props.label}
+            //                                 onChange={event => this.handleChange(event)}
+            //                                 inputProps={{
+            //                                     style: {
+            //                                         fontSize: '18px',
+            //                                         backgroundImage: 'none',
+            //                                         width: '100px'
+            //                                     },
+            //                                     disabled: true
+            //                                 }}
+            //                                 open={this.state.showDatePicker}
+            //                             />
+            //                         </FormControl>
+            //                     </GridItem>
+            //                 ) : (
+            //                     <GridItem>
+            //                         <div className={classes.label}>{this.props.label}</div>
+            //                     </GridItem>
+            //                 )
+            //             }
+            //             {
+            //                 this.props.view === "day" && 
+            //                     <GridItem className={classes.pl_0}>
+            //                         <ClickAwayListener onClickAway={() => this.setState({showDatePicker: false})}>
+            //                             <Button
+            //                                 simple
+            //                                 color="info"
+            //                                 className={classes.actionButton + " " + classes.actionButtonRound}
+            //                                 onClick={() => this.changeDate()}
+            //                             >
+            //                                 <Today />
+            //                             </Button>
+            //                         </ClickAwayListener>
+            //                     </GridItem>
+            //             }
+            //             <GridItem>
+            //                 <Button
+            //                     simple
+            //                     color="info"
+            //                     className={classes.actionButton + " " + classes.actionButtonRound}
+            //                     onClick={() => onNavigate('NEXT')}
+            //                 >
+            //                     <ArrowForwardIos />
+            //                 </Button>
+            //             </GridItem>
+            //         </GridContainer>
+            //     </GridItem>
+            //     <GridItem xs={12} sm={4} className={classes.right}>
+            //         <Button color="success" round size="sm" onClick={() => onView('month')} className={classes.marginRight}>
+            //             Month
+            //         </Button>
+            //         <Button color="success" round size="sm" onClick={() => onView('week')} className={classes.marginRight}>
+            //             Week
+            //         </Button>
+            //         <Button color="success" round size="sm" onClick={() => onView('day')}>
+            //             Day
+            //         </Button>
+            //     </GridItem>
+            // </GridContainer>         
+            <GridContainer justify="center" alignItems="center" className={classes.pb_15}>
+                <GridItem>
+                    <Button
+                        simple
+                        color="info"
+                        className={classes.actionButton + " " + classes.actionButtonRound}
+                        onClick={() => onNavigate('PREV')}
+                    >
+                        <ArrowBackIos />
                     </Button>
-                    <Button color="success" round size="sm" onClick={() => onView('week')} className={classes.marginRight}>
-                        Week
-                    </Button>
-                    <Button color="success" round size="sm" onClick={() => onView('day')}>
-                        Day
-                    </Button>
                 </GridItem>
-            </GridContainer>           
+                <GridItem className={classes.pr_0}>
+                    <FormControl>
+                        <Datetime
+                            dateFormat={"YYYYMMDD"}
+                            timeFormat={false}
+                            value={this.props.label}
+                            onChange={event => this.handleChange(event)}
+                            inputProps={{
+                                style: {
+                                    fontSize: '18px',
+                                    backgroundImage: 'none',
+                                    width: '100px'
+                                },
+                                disabled: true
+                            }}
+                            open={this.state.showDatePicker}
+                        />
+                    </FormControl> 
+                </GridItem>
+                <GridItem className={classes.pl_0}>
+                    <ClickAwayListener onClickAway={() => this.setState({showDatePicker: false})}>
+                        <Button
+                            simple
+                            color="info"
+                            className={classes.actionButton + " " + classes.actionButtonRound}
+                            onClick={() => this.changeDate()}
+                        >
+                            <Today />
+                        </Button>
+                    </ClickAwayListener> 
+                 </GridItem>
+                <GridItem>
+                    <Button
+                        simple
+                        color="info"
+                        className={classes.actionButton + " " + classes.actionButtonRound}
+                        onClick={() => onNavigate('NEXT')}
+                    >
+                        <ArrowForwardIos />
+                     </Button>
+                 </GridItem>
+            </GridContainer>  
         );
     }
 }
